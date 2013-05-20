10406c808f25d059167964c9da1ffe24305dc7e0
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-20 11:43:54 +0530

    7th Problem Solved

--- 7.py
+++ 7.py
@@ -2,40 +2,37 @@
 
 import math
 
-def main() :
-  print nthprime(100001)      
 
-def nthprime(n):
-         counter = 1
-	if n==1: 
-	  return 2
-	elif n<1: 
-	  return None
-	prime = 2
-	while counter <= n :
-	  while 1:
-	    a = 2
-	    s=range(3,a+1,2)
-	    counter = 1
-	    root = a ** 0.5
-	    half=(a+1)/2-1
-	    i=0
-	    r=3
-	    while r <= root:
+def main() :
+  i = 1
+  while True :
+    index = 10001
+    a = primes(i)
+    if len(a) > index-1 :
+     ans = a[index-1]
+     break
+    else : 
+     i = i*10
+  print ans
+
+def primes(n): 
+	if n==2: return [2]
+	elif n<2: return []
+	s=range(3,n+1,2)
+	root = n ** 0.5
+	half=(n+1)/2-1
+	i=0
+	r=3
+	while r <= root:
 		if s[i]:
 			j=(r*r-3)/2
 			s[j]=0
-			counter = counter +1
 			while j<half:
 				s[j]=0
 				j+=r
-			        counter = counter +1
-	        i=i+1
+		i=i+1
 		r=2*i+3
-	  if counter == n :
-	    break
-		
-	return a
+	return [2]+[x for x in s if x]
       
 if __name__ == "__main__" :
   main()  
