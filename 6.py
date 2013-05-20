20248117e58bd6a8b8fdaeb1585489b8a24c33b5
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-20 10:44:29 +0530

    6th Problem Solution

--- 6.py
+++ 6.py
@@ -3,19 +3,13 @@
 import math
 
 def main() :
-  mult = 1
-  for i in range(1,20) :
-    a = gcd(mult,i)
-    if a == 0 :
-      a = 1
-    mult = mult*(i/a)
-  print(mult)  
-         
-def gcd(a,b):
-        if b == 0:
-                return a
-        else:
-                return gcd(b, (a%b))      
+  a = 0
+  b = 0
+  for i in range(1,101) :
+    a = a + i
+    b = b + i*i
+  a = a*a
+  print (a - b)           
       
 if __name__ == "__main__" :
   main()   
