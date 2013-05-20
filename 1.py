adbfb868e5491f710a2b35dd387c6a337a3b13de
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-20 08:42:09 +0530

    Finish Problem 1

--- 1.py
+++ 1.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 def main() :
-  for i in range(1001):
-    print(i)
-
+  x = 0
+  for i in range(1000):
+    rem3 = i%3
+    rem5 = i%5
+    if rem3 == 0:
+      x=x+i
+    elif rem5 == 0:
+      x=x+i
+  print(x)               
 
 if __name__ == "__main__" :
   main() 
