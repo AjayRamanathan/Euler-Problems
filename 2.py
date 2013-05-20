d1f3014c13ba592a6ac7c7af4554b5e4fa61f1df
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-20 09:00:42 +0530

    Finished problem 2: http://projecteuler.net/problem=2

---
+++ 2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+def main() :
+  x = 0
+  a = 1
+  b = 1
+  for i in range(100) :
+    a = a+b
+    b = b+a
+    rema = a%2
+    remb = b%2
+    if rema == 0 :
+      x = x + a
+    if remb == 0 :
+      x = x + b
+    if a > 4000000 :
+      break
+    if b > 4000000 :
+      break
+  print x
+if __name__ == "__main__" :
+  main()  
+
+
+#4000000
