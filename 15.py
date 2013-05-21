33097e48644afb4a4c5f9b996fdffc064e226ef8
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-21 09:51:33 +0530

    14th working,needs bottom up approach, too much effort, 15th was too easy !

---
+++ 15.py
@@ -0,0 +1,3 @@
+ #This problem is too easy, the answer is 40!/20!*20!
+ #As you have 20 rigths and 20 downwards, rearranging them !
+ #I might write brute force or a permutation solution, Maybe latter !
