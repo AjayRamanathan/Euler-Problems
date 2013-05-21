d57a4c29b6a37dbfa9f379109358222f12ae4e7e
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-21 14:40:13 +0530

    419 is working but not for above n = 100; it shows memory error

--- 419.py
+++ 419.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 def main() :
-  a = '111221'
-  print(digpart(a))
+  
+  a = '1'
+  i = 1
+  n = 50
+  #n = 10**12
+  print n
+  while i < n :
+    a = digpart(a)
+    i = i +1 
+  print(abc(a)) 
     
 def digpart(a) :
   
@@ -18,18 +26,29 @@ def digpart(a) :
       b.append(counter)
       b.append(int(x))
       counter = 0
-      x = a[:j-1]
+      x = a[j-1:j]
       counter = 1
     j = j+1
-    if counter == 0 :
-      continue
-    elif j == length+1 :
+    if j == length+1 :
       b.append(counter)
       b.append(int(x))
   str1 = ''.join(str(e) for e in b)
   return str1
-    
-	
+
+def abc(a) :
+  a1 = 0 
+  b1 = 0
+  c1 = 0
+  for i in a :
+    if i == '1':
+      a1 = a1 + 1
+    elif i == '2':
+      b1 = b1 + 1
+    elif i == '3':
+      c1 = c1 + 1
+  ex = [a1, b1, c1] 
+  return ex
+
 	
 if __name__ == "__main__" :
   main()  
