228890e240f64194bd095b6fbc7e53cf03c8e724
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-21 12:47:46 +0530

    419 still needs debgging but close enuf

---
+++ 419.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+def main() :
+  a = '21'
+  print(digpart(a))
+    
+def digpart(a) :
+  
+  b = []
+  length = len(a)
+  x = a[:1]
+  counter = 0
+  j = 1
+  for i in a :
+    if i == x :
+      counter = counter + 1
+    else :
+      b.append(counter)
+      b.append(int(x))
+      counter = 0
+      x = a[:j+1]
+    j = j+1
+    if counter == 0:
+      continue
+    elif j == length+1 :
+      b.append(counter)
+      b.append(int(x))
+  str1 = ''.join(str(e) for e in b)
+  return str1
+    
+	
+	
+if __name__ == "__main__" :
+  main()  
