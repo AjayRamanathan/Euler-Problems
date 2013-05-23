000c324c4b04bfd0335ebd9690cb469be7a4d68f
Ajay Ramanathan <ajay.010293@gmail.com>
2013-05-23 09:18:06 +0530

    12th debugged

--- 12.py
+++ 12.py
@@ -5,47 +5,21 @@ def main() :
   a = 0
   n = 1
   while a <= 500 :
-    a = numdiv (nthtri(n))
-    print a
-  print n
-    
-def numdiv (n) :
-  rem = 0
-  a = []
-  list1 = primes (n)
-  for i in list1 :
-    x = 0
-    while rem != 0 :
-      n = n/i
-      x = x + 1
-    a.append(x)
-  return a
+    x = tri(n)
+    y = factors(x)
+    a = len(y)
+    n = n +1
+  print  x
+  
+def tri(n) :
+  total = 0
+  for i in range(1, n+1) :
+    total = total + i
+  return total  
 
-
-def nthtri(n) :
-  x = 0
-  for i in range (1, n+1) :
-    x = x + i
-  return x  
-
-def primes(n): 
-	if n==2: return [2]
-	elif n<2: return []
-	s=range(3,n+1,2)
-	root = n ** 0.5
-	half=(n+1)/2-1
-	i=0
-	r=3
-	while r <= root:
-		if s[i]:
-			j=(r*r-3)/2
-			s[j]=0
-			while j<half:
-				s[j]=0
-				j+=r
-		i=i+1
-		r=2*i+3
-	return [2]+[x for x in s if x]
+def factors(n):
+    return set(x for tup in ([i, n//i] 
+                for i in range(1, int(n**0.5)+1) if n % i == 0) for x in tup)
 
 if __name__ == "__main__" :
   main()  
